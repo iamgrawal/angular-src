923088b838ccc97f71bbfaebbb6ac47da9fa99af
Gaurav Rawal <gaurav.rawal66@gmail.com>
2017-12-21 13:05:02 +0530

    Registration Functionality Added

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ValidateService } from '../../services/validate.service';
+import { AuthService } from '../../services/auth.service';
 import { FlashMessagesService } from 'ngx-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -14,7 +16,12 @@ export class RegisterComponent implements OnInit {
 	email: String;
 	password: String;
 
-  constructor(private validateService: ValidateService, private flashMessage: FlashMessagesService) { }
+  constructor(
+  	private validateService: ValidateService,
+   	private flashMessage: FlashMessagesService,
+   	private authService: AuthService,
+   	private router: Router
+   ) { }
 
   ngOnInit() {
   }
@@ -37,6 +44,17 @@ export class RegisterComponent implements OnInit {
   			this.flashMessage.show('Please use a valid Email', {classes: ['alert', 'alert-warning'], timeout: 3000});
   			return false;
   	}
+  	//Register User
+  	this.authService.registerUser(user).subscribe(data => {
+  		if(data.success){
+  			this.flashMessage.show('You are now registered and can log in', {classes: ['alert', 'alert-success'], timeout: 3000});
+  			this.router.navigate(['/login']);
+  		} else{
+  			this.flashMessage.show('Something went wrong', {classes: ['alert', 'alert-success'], timeout: 3000});
+  			this.router.navigate(['/register']);
+  		}
+
+  	});
   }
 
 }

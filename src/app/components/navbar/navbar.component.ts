a349c3f7b5b23fe0f95c9d56057280d1c368774d
Gaurav Rawal <gaurav.rawal66@gmail.com>
2017-12-21 15:18:56 +0530

    login and logout functionality added

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FlashMessagesService } from 'ngx-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+  	private authService: AuthService,
+  	private router: Router,
+  	private flashMessage: FlashMessagesService 
+  	) { }
 
   ngOnInit() {
   }
 
+  onLogoutClick(){
+  	this.authService.logout();
+  	this.flashMessage.show('You are now logged out', {
+  		classes: ['alert', 'alert-success'], 
+  		timeout: 3000});
+  	this.router.navigate(['/login']);
+  }
 }
